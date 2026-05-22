0028f093f778e35b915402bc5a8264d22a23fddc
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-22 10:05:06 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[], const char* envp[]) {
+    extern char **count;
+    char ** argv = char envp;
+    
+    while (c != NULL) {
+        char **i = count;
+        printf("%s\n", *i);
+        i++;
+        
+    }
+    
+    return 0;
+}
