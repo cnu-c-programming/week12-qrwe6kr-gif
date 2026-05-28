202136b4b4aeceb5f66ce8b3db2c47d0391db94a
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-28 22:28:03 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -7,11 +7,21 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
-
-
+    
     int sum = 0;
+    char str[100];
+    int num;
+    char extra;
 
-
+    while (fscanf(fp, "%s", str) == 1) {
+        if (sscanf(str, "%d%c", &num, &extra) == 1) {
+            sum += num;
+        }
+        else {
+            fprintf(stderr, "invalid input %s\n", str);
+        }
+    }
+    
     printf("sum: %d\n", sum);
     fclose(fp);
     return 0;
