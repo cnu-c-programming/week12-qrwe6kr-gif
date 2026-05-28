97ce1582940cdcf5c5fb26c5213f653ba563c211
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-28 22:33:10 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,20 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    while (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+    count++;
+    }
 
     int max = 0;
     float avg = 0;
 
+    for (int i = 0; i < count; i++) {
+    if (students[i].score > max)
+        max = students[i].score;
 
+    avg += students[i].score;
+    }
+    avg /= count;
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
