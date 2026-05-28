548470a52590894713a6c76c29f2698a9727044c
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-28 22:07:43 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,18 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    int count[26] = {0};
+    int ch;
 
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch >= 'a' && ch <= 'z') {
+            count[ch - 'a']++;
+        }
+    }
 
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
     fclose(fp);
 }
 
