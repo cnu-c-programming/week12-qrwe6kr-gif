5fa71511f193b4e302700651a508f2f4696bbae1
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-28 22:09:14 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,15 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    int ch;
+    while ((ch = fgetc(fp_src)) != EOF) {
+        fputc(ch, fp_dst);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
 
