caaa8c97b5c4ea4951de866f39325c4521d4e8ef
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-05-28 22:41:29 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -4,10 +4,8 @@ int main(int argc, const char* argv[], const char* envp[]) {
     int i = 0;
     
     while (envp[i] != NULL) {
-        char **i = count;
         printf("%s\n", envp[i]);
         i++;
-        
     }
     
     return 0;
